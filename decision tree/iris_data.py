e44f982860365519c10c36032532727d5d97c30e
kamujusrujan <kamujusrujan01@gmail.com>
2020-10-24 00:28:45 +0530

    test 10 iterations over iris accuracy ranges from 90-100 %

--- decision tree/iris_data.py
+++ decision tree/iris_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from  decision_tree import DecisionTree	
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 df = pd.read_pickle('modified_iris.pkl')
 features = []
@@ -10,14 +11,19 @@ for i in range(len(df)):
 
 
 def start():
-	X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+	X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+	print(y_test)
 	dt = DecisionTree()
 	dt.fit(X_train, y_train)
 	true = 0
 	for i in range(len(X_test)):
 		# print(y_test[i])
 		true +=1 if dt.predict(X_test[i]) == y_test[i] else 0
-	print(true/len(y_test)  )
+	return true/len(y_test)
 	# print(dt.to_string())
 
-start()
+
+avg = []
+for i in range(10):
+	avg.append(start())
+print(avg,np.mean(avg))
